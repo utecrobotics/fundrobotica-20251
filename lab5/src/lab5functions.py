157e26e53e212166cadc5a7985babceb3a289999
Alexander López <lopez.manrique.alexander@gmail.com>
2025-05-22 18:12:53 -0500

    Add files via upload

---
+++ lab5/src/lab5functions.py
@@ -0,0 +1,142 @@
+import numpy as np
+from copy import copy
+
+cos=np.cos; sin=np.sin; pi=np.pi
+
+
+def dh(d, theta, a, alpha):
+ """
+ Calcular la matriz de transformacion homogenea asociada con los parametros
+ de Denavit-Hartenberg.
+ Los valores d, theta, a, alpha son escalares.
+ """
+ # Escriba aqui la matriz de transformacion homogenea en funcion de los valores de d, theta, a, alpha
+ T = 0
+ return T
+    
+    
+
+def fkine(q):
+ """
+ Calcular la cinematica directa del brazo robotico dados sus valores articulares. 
+ q es un vector numpy de la forma [q1, q2, q3, ..., qn]
+ """
+ # Longitudes (en metros)
+
+ # Matrices DH (completar), emplear la funcion dh con los parametros DH para cada articulacion
+ T1 = 0
+ T2 = 0
+ T3 = 0
+ T4 = 0
+ T5 = 0
+ # Efector final con respecto a la base
+ T = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
+ return T
+
+
+def jacobian_position(q, delta=0.0001):
+ """
+ Jacobiano analitico para la posicion de un brazo robotico de n grados de libertad. 
+ Retorna una matriz de 3xn y toma como entrada el vector de configuracion articular 
+ q=[q1, q2, q3, ..., qn]
+ """
+ # Determinar la cantidad de articulaciones
+ #n =
+ # Crear una matriz 3xn 
+ #J = 
+ # Calcular la transformacion homogenea inicial (usando q)
+ 
+    
+ # Iteracion para la derivada de cada articulacion (columna)
+ for i in range(n):
+  # Copiar la configuracion articular inicial
+  dq = copy(q)
+  # Calcular nuevamenta la transformacion homogenea e
+  # Incrementar la articulacion i-esima usando un delta, 
+  # usar la copia de configuración inicial
+  
+  # Transformacion homogenea luego del incremento (q+delta)
+  
+  # Aproximacion del Jacobiano de posicion usando diferencias finitas
+
+ return J
+
+
+def ikine(xdes, q0):
+ """
+ Calcular la cinematica inversa de un brazo robotico numericamente a partir 
+ de la configuracion articular inicial de q0. Emplear el metodo de newton.
+ """
+ epsilon  = 0.001
+ max_iter = 1000
+ delta    = 0.00001
+
+ q  = copy(q0)
+ for i in range(max_iter):
+  # Main loop
+  pass
+    
+ return q
+
+
+def ik_gradient(xdes, q0):
+ """
+ Calcular la cinematica inversa de un brazo robotico numericamente a partir 
+ de la configuracion articular inicial de q0. Emplear el metodo gradiente.
+ """
+ epsilon  = 0.001
+ max_iter = 1000
+ delta    = 0.00001
+
+ q  = copy(q0)
+ for i in range(max_iter):
+  # Main loop
+  pass
+    
+ return q
+
+    
+def rot2quat(R):
+ """
+ Convertir una matriz de rotacion en un cuaternion
+
+ Entrada:
+  R -- Matriz de rotacion
+ Salida:
+  Q -- Cuaternion [ew, ex, ey, ez]
+
+ """
+ dEpsilon = 1e-6
+ quat = 4*[0.,]
+
+ quat[0] = 0.5*np.sqrt(R[0,0]+R[1,1]+R[2,2]+1.0)
+ if ( np.fabs(R[0,0]-R[1,1]-R[2,2]+1.0) < dEpsilon ):
+  quat[1] = 0.0
+ else:
+  quat[1] = 0.5*np.sign(R[2,1]-R[1,2])*np.sqrt(R[0,0]-R[1,1]-R[2,2]+1.0)
+ if ( np.fabs(R[1,1]-R[2,2]-R[0,0]+1.0) < dEpsilon ):
+  quat[2] = 0.0
+ else:
+  quat[2] = 0.5*np.sign(R[0,2]-R[2,0])*np.sqrt(R[1,1]-R[2,2]-R[0,0]+1.0)
+ if ( np.fabs(R[2,2]-R[0,0]-R[1,1]+1.0) < dEpsilon ):
+  quat[3] = 0.0
+ else:
+  quat[3] = 0.5*np.sign(R[1,0]-R[0,1])*np.sqrt(R[2,2]-R[0,0]-R[1,1]+1.0)
+
+ return np.array(quat)
+
+
+def TF2xyzquat(T):
+ """
+ Convert a homogeneous transformation matrix into the a vector containing the
+ pose of the robot.
+
+ Input:
+  T -- A homogeneous transformation
+ Output:
+  X -- A pose vector in the format [x y z ew ex ey ez], donde la first part
+       is Cartesian coordinates and the last part is a quaternion
+ """
+ quat = rot2quat(T[0:3,0:3])
+ res = [T[0,3], T[1,3], T[2,3], quat[0], quat[1], quat[2], quat[3]]
+ return np.array(res)
